dc9dbd9933395d1db36fac8fce59c0f0eebb9f1c
rhythmkhanderia-tech <rhythmkhanderia@gmail.com>
2026-03-12 13:44:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 VERIFY_TOKEN = "ridham_ai"
 
@@ -33,5 +33,5 @@ def webhook():
 
 
 # Run server
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
