a487ac3feb0f9031d39d73525b253d5fbd51f8d2
rhythmkhanderia-tech <rhythmkhanderia@gmail.com>
2026-03-12 13:38:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, request
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 VERIFY_TOKEN = "ridham_ai"
 
+@app.route("/")
+def home():
+    return "Server running"
+
 @app.route("/webhook", methods=["GET","POST"])
 def webhook():
-    
+
     if request.method == "GET":
+        mode = request.args.get("hub.mode")
         token = request.args.get("hub.verify_token")
         challenge = request.args.get("hub.challenge")
 
-        if token == VERIFY_TOKEN:
-            return challenge
-        return "Verification token mismatch", 403
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            return challenge, 200
+        else:
+            return "Verification failed", 403
 
     if request.method == "POST":
         data = request.json
@@ -21,5 +27,5 @@ def webhook():
         return "EVENT_RECEIVED", 200
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     app.run(host="0.0.0.0", port=5000)
